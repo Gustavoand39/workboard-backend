09ff4b9cb2e4b85873de93fd97e70716d3b73f24
Gustavoand39 <111763968+Gustavoand39@users.noreply.github.com>
2024-10-24 17:41:26 -0600

    docs: Agregar documentación a hashedPassword

--- src/utils/hashedPassword.ts
+++ src/utils/hashedPassword.ts
@@ -1,11 +1,27 @@
-import bcrypt from 'bcryptjs';
+import bcrypt from "bcryptjs";
 
 const SALT_ROUNDS = 10;
 
+/**
+ * Encripta la contraseña del usuario
+ *
+ * @param {string} password - Contraseña del usuario
+ * @returns {Promise<string>} - Contraseña encriptada
+ */
 export const hashedPassword = async (password: string): Promise<string> => {
   return bcrypt.hash(password, SALT_ROUNDS);
-}
+};
 
-export const comparePassword = async (password: string, hashedPassword: string): Promise<boolean> => {
+/**
+ * Compara la contraseña del usuario con la contraseña encriptada
+ *
+ * @param {string} password - Contraseña del usuario
+ * @param {string} hashedPassword - Contraseña encriptada
+ * @returns {Promise<boolean>} - Si la contraseña es correcta
+ */
+export const comparePassword = async (
+  password: string,
+  hashedPassword: string
+): Promise<boolean> => {
   return bcrypt.compare(password, hashedPassword);
-}
+};

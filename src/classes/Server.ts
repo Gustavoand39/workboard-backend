48258c34d2e3759ce4c34cbc0ed471f22073f9e9
Gustavoand39 <111763968+Gustavoand39@users.noreply.github.com>
2024-10-23 19:42:16 -0600

    chore: Agregar archivos iniciales

---
+++ src/classes/Server.ts
@@ -0,0 +1,46 @@
+import express, { type Application } from "express";
+import { createServer, Server as httpServer } from "http";
+import cors from "cors";
+
+export class Server {
+  private static instance: Server;
+  private app: Application;
+  private httpServer: httpServer;
+  private port: number;
+
+  constructor() {
+    this.app = express();
+    this.httpServer = createServer(this.app);
+    this.port = parseInt(process.env.SERVER_PORT || "3001");
+
+    this.initMiddlewares();
+    this.initRoutes();
+  }
+
+  public static getInstance(): Server {
+    if (!Server.instance) {
+      this.instance = new Server();
+    }
+    return this.instance;
+  }
+
+  private initMiddlewares(): void {
+    this.app.use(cors({
+      origin: "*",
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: true,
+    }));
+    this.app.use(express.json());
+  }
+
+  private initRoutes(): void {
+    // Add routes here
+  }
+
+  public start(): void {
+    this.httpServer.listen(this.port, () => {
+      console.log(`Servidor corriendo en http://localhost:${this.port}`);
+    });
+  }
+}
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+import { Server } from "@/classes/Server";
+
+const server = Server.getInstance();
+
+server.start();
